ca65619764ee79ace238920db22d971efca08773
Muneeb Anjum <tmuneebanjum@gmail.com>
2021-07-18 09:43:57 +0000

    Updates

---
+++ precache-manifest.7f36a6cc2da412c10bfdfea7180ddb8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77269b5877b95477a8cbfb9a689428e8",
+    "url": "/filesave/index.html"
+  },
+  {
+    "revision": "d46a5e55a131715241fe",
+    "url": "/filesave/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "e61bee5d8af41e81eac6",
+    "url": "/filesave/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "d46a5e55a131715241fe",
+    "url": "/filesave/static/js/2.bc8f9413.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/filesave/static/js/2.bc8f9413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61bee5d8af41e81eac6",
+    "url": "/filesave/static/js/main.b2228519.chunk.js"
+  },
+  {
+    "revision": "7d8c98e749f8ad834ce6",
+    "url": "/filesave/static/js/runtime-main.56929fa4.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/filesave/static/media/box.df8b84d5.png"
+  }
+]);
